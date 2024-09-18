c05ca826f6d55e243278bbd486033730aaf2ce3e
Mikhail Voloshin (desktop workstation) <mvol@live.com>
2024-09-18 10:55:00 -0400

    Set up a simple Hello World scaffold.

--- infoservant/infoservant_impl.py
+++ infoservant/infoservant_impl.py
@@ -6,13 +6,13 @@ import webpage2content
 from bs4 import BeautifulSoup
 
 from typing import Optional, Union, List
-from contextlib import contextmanager
+
+LOGGER = logging.getLogger("infoservant")
 
 
 def _call_gpt(
     conversation: Union[str, dict, List[dict]],
     openai_client: openai.OpenAI,
-    logger: Optional[logging.Logger] = None,
 ) -> str:
     if isinstance(conversation, str):
         conversation = [{"role": "user", "content": conversation}]
@@ -21,12 +21,11 @@ def _call_gpt(
 
     answer_full = ""
     while True:
-        if logger:
-            logger.debug(
-                f"webpage2content._call_gpt calling chat completion "
-                f"with conversation of {len(conversation)} messages. "
-                f"Last message is {len(conversation[-1]['content'])} chars long."
-            )
+        LOGGER.debug(
+            f"infoservant._call_gpt calling chat completion "
+            f"with conversation of {len(conversation)} messages. "
+            f"Last message is {len(conversation[-1]['content'])} chars long."
+        )
 
         completion = openai_client.chat.completions.create(
             model="gpt-4o",
@@ -37,11 +36,10 @@ def _call_gpt(
         answer = completion.choices[0].message.content
         answer_full += answer + "\n"
 
-        if logger:
-            logger.debug(
-                f"webpage2content._call_gpt got answer of length {len(answer)}, "
-                f"appending to full answer currently at length {len(answer_full)}"
-            )
+        LOGGER.debug(
+            f"infoservant._call_gpt got answer of length {len(answer)}, "
+            f"appending to full answer currently at length {len(answer_full)}"
+        )
 
         conversation.append(
             {
@@ -49,18 +47,16 @@ def _call_gpt(
                 "content": answer,
             }
         )
-        conversation.append(
-            {
-                "role": "user",
-                "content": "Please continue from where you left off.",
-            }
-        )
 
         if completion.choices[0].finish_reason == "length":
-            if logger:
-                logger.debug(
-                    "webpage2content._call_gpt finish reason length, continuing loop"
-                )
+            conversation.append(
+                {
+                    "role": "user",
+                    "content": "Please continue from where you left off.",
+                }
+            )
+
+            LOGGER.debug("infoservant._call_gpt finish reason length, continuing loop")
             continue
 
         break
@@ -69,136 +65,72 @@ def _call_gpt(
     return answer_full
 
 
-def webpage2content(
-    url: str,
+def infoservant(
+    command: str,
     openai_client: openai.OpenAI,
-    logger: Optional[logging.Logger] = None,
+    serp_api_key: str = "",
 ):
-    if not logger:
-        logger = logging.getLogger(__name__)
-
-    markdown = _get_page_as_markdown(url, logger=logger)
-    if not markdown:
-        return None
-
-    if not isinstance(markdown, str):
-        logger.error("markdown somehow came back as something other than a string.")
-        return None
-
-    markdown = markdown.strip()
-    if not markdown:
-        return None
-
-    mdlines = markdown.splitlines()
-    mdlines = [f"{linenum+1}. {linetext}" for linenum, linetext in enumerate(mdlines)]
-    markdown_with_linenums = "\n".join(mdlines)
-
-    # TODO: Break up the markdown into pieces if the webpage is too big.
-    conversation = [
-        {"role": "system", "content": SYSTEMPROMPT},
-        {"role": "user", "content": markdown_with_linenums},
-    ]
-
-    # First, we get the AI to describe the page to us in its own words.
-    # We are uninterested in this answer. We just want it to have this conversation
-    # with itself so that it knows what's going to be important in subsequent steps.
-    try:
-        logger.debug(f"webpage2content is asking GPT to describe {url}")
-        gptreply_page_description = _call_gpt(
-            conversation=conversation,
-            openai_client=openai_client,
-            logger=logger,
-        )
-        logger.debug(f"webpage2content asked GPT to describe {url}")
-        conversation.append({"role": "assistant", "content": gptreply_page_description})
-    except Exception:
-        logger.exception("Exception in webpage2content determining content type")
-        return None
-
-    # Next, we simply ask it whether or not the content is human-readable.
-    try:
-        logger.debug(f"webpage2content is determining human readability of {url}")
-        conversation.append({"role": "user", "content": PROMPT_HUMAN_READABLE_CHECK})
-        gptreply_is_human_readable = _call_gpt(
-            conversation=conversation,
-            openai_client=openai_client,
-            logger=logger,
-        )
-
-        is_human_readable = "yes" in gptreply_is_human_readable.lower()
-        if not is_human_readable:
-            logger.warning(f"Page at URL {url} is not human-readable")
-            return None
-        else:
-            logger.debug(f"webpage2content confirmed human readability of {url}")
-
-    except Exception:
-        logger.exception("Exception in webpage2content checking human readability")
-        return None
-
-    # At last, we call it with the line filtration prompt.
-    try:
-        logger.debug(f"webpage2content is querying line filtration for {url}")
-        conversation[-1] = {"role": "user", "content": PROMPT_LINE_FILTER}
-        gptreply_line_filtration = _call_gpt(
-            conversation=conversation,
-            openai_client=openai_client,
-            logger=logger,
-        )
-        logger.debug(f"webpage2content has queried line filtration for {url}")
-
-    except Exception:
-        logger.exception("Exception in webpage2content choosing lines to filter")
-        return None
-
-    mdlines = markdown.splitlines()
-    filterlines = gptreply_line_filtration.splitlines()
-
-    logger.debug(f"webpage2content is iterating through line filtration for {url}")
-
-    for filterline in filterlines:
-        try:
-            linenumstr, linetext = filterline.split(".", maxsplit=1)
-            linenum = int(linenumstr) - 1
-
-            if linetext.lower().endswith("discard"):
-                mdlines[linenum] = ""
-
-        except Exception as ex:
-            logger.debug(f"Nonthreatening exception during line filtration: {ex}")
-            pass
-
-    markdown = "\n".join(mdlines)
-    markdown = re.sub(r"\n\n\n+", "\n\n", markdown)
-    markdown = markdown.strip()
-
-    logger.debug(f"webpage2content has constructed filtered markdown for {url}")
-    return markdown
+    print("Hello World")
 
 
 def main():
-    if len(sys.argv) < 2 or len(sys.argv) > 3:
-        print("Usage: webpage2content <URL> [OPENAI_API_KEY]")
-        sys.exit(1)
+    import argparse
 
-    url = sys.argv[1]
+    parser = argparse.ArgumentParser(
+        description=(
+            "An AI that browses text content on the web. Easily integrate intelligent web surfing into any project."
+        )
+    )
+    parser.add_argument(
+        "-l",
+        "--log-level",
+        help="Sets the logging level. (default: %(default)s)",
+        type=str,
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+    )
+    parser.add_argument(
+        "-k",
+        "--key",
+        help="OpenAI API key. If not specified, reads from the environment variable OPENAI_API_KEY.",
+        type=str,
+    )
+    parser.add_argument(
+        "-o",
+        "--org",
+        help="OpenAI organization ID. If not specified, reads from the environment variable OPENAI_ORGANIZATION. "
+        "If no such variable exists, then organization is not used when calling the OpenAI API.",
+        type=str,
+    )
+    parser.add_argument(
+        "-s",
+        "--serpapi",
+        help="SerpApi key. If not provided, will run without SerpApi capabilities.",
+        type=str,
+    )
+    parser.add_argument(
+        "-c",
+        "--command",
+        help=(
+            "The instructions to resolve via web browsing. These are typically a short plain-English "
+            "statement of information to find, a question to answer, or even just the URL of a "
+            "web page to retrieve."
+        ),
+        type=str,
+    )
 
-    OPENAI_API_KEY = None
-    if len(sys.argv) > 2:
-        OPENAI_API_KEY = sys.argv[2]
+    args = parser.parse_args()
 
-    openai_client = None
-    if OPENAI_API_KEY:
-        openai_client = openai.OpenAI(api_key=OPENAI_API_KEY)
-    else:
-        openai_client = openai.OpenAI()
+    if args.log_level:
+        log_level = logging.getLevelName(log_level)
+        LOGGER.setLevel(log_level)
 
-    try:
-        content = webpage2content(url, openai_client=openai_client)
-        print(content)
-    except Exception as e:
-        print(f"Error: {e}")
-        sys.exit(1)
+    s = infoservant(
+        openai_api_key=args.key,
+        openai_org_id=args.org,
+        serp_api_key=args.serpapi or "",
+    )
+    print(s)
 
 
 if __name__ == "__main__":
