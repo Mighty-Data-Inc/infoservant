2d469904281b22b947a8fdfce72911a099d64835
Mikhail Voloshin (desktop workstation) <mvol@live.com>
2024-09-21 19:48:33 -0400

    Gotta install python packages for distribution.

--- deploy.ps1
+++ deploy.ps1
@@ -2,5 +2,6 @@
 powershell.exe -noninteractive -command remove-item build -recurse
 powershell.exe -noninteractive -command remove-item dist -recurse
 powershell.exe -noninteractive -command remove-item *.eggo-info -recurse
+pip install wheel twine
 python setup.py sdist bdist_wheel
 twine upload dist/* --verbose
